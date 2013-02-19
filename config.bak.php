8e2d4e737225210c359116ff2f6fbb155e064af2
raihansikder <raihan.act@gmail.com>
2013-02-19 16:22:14 +0600

    #

---
+++ config.bak.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE );
+session_start();
+
+include_once('inc.globalvariables.php');
+include_once('inc.functions.generic.php');
+include_once('inc.functions.appspecific.php');
+
+
+
+?>
