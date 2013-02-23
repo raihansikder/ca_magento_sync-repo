dfcb2f47e6d319aae1862826bbdcbe7dec1cdda4
Raihan Sikder <raihan.act@gmail.com>
2013-02-23 16:35:55 -0500

    cloud9 commit
    3

--- index.php
+++ index.php
@@ -3,5 +3,4 @@ include_once('config.php');
 //$result = $client->DoesSkuExist(array("accountID" => "700cd2eb-ff3c-4969-a542-dfde923e14fd","sku" => "FRE3216SLECHILD5"));
 //print $result->DoesSkuExistResult->Status;
 //myprint_o(GetInventoryItemQuantityInfo($accountID,'FRE3216SLECHILD5'));
-
 ?>
